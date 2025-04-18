d32436545084ee1e9393a79f3c6b6f62fea4501f
Sefacan Demir <sefacandmir@gmail.com>
2025-04-18 15:18:18 +0300

    Initial Commit

---
+++ TodoAppAssigment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoAppAssigment2
+//
+//  Created by Ulgen on 18.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
